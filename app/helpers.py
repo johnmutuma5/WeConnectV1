8a07a4247d3411b67dbf3bdd75dc32d4a1e9c6ef
John Mutuma <johnmutuma5@gmail.com>
2018-04-25 08:07:08 +0300

    Add function for inspecting user input

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,5 @@
 import random
+from .exceptions import MissingDataError
 
 def generate_token ():
     chars = ""
@@ -11,6 +12,20 @@ def generate_token ():
 
     token = ""
     for i in range(96):
-        rand_index = random.randint(0, 51)
+        rand_index = random.randint(0, 61)
         token += chars[rand_index]
     return token
+
+
+def inspect_data(required_fields, data):
+    '''
+        Removes extra spaces in data and checks for blank fields
+    '''
+    for field in required_fields:
+        field_value = data.get(field, "").strip()
+        if not len(field_value):
+            raise MissingDataError(msg='Please provide %s' %field)
+        field_value = " ".join(field_value.split())
+        data[field] = field_value
+
+    return data
