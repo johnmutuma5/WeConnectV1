f3df1646db582068e35d2d57ab3faf3ddfefe226
John Mutuma <johnmutuma5@gmail.com>
2018-04-25 08:07:50 +0300

    Add MissDataError class

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -32,6 +32,14 @@ class InvalidUserInputError (Exception):
         class: DuplicationError
         raised: when trying to read/write unauthorised data
     '''
-    def __init__ (self, expression, msg):
+    def __init__ (self, expression=None, msg=None):
         self.expression = expression
         self.msg = msg
+
+class MissingDataError(InvalidUserInputError):
+    '''
+        class: MissingDataError
+        raised: when there are blank fields in data
+    '''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
